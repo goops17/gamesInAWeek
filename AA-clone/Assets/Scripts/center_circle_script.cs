2e00146c48d0d4332b0955ea4aad0d1f8c2dce9f
Gopal Chitaure <gchitaure@wv.local>
2019-02-13 14:18:01 -0600

    AA-clone basic gameplay

---
+++ AA-clone/Assets/Scripts/center_circle_script.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class center_circle_script : MonoBehaviour
+{
+    float rotationSpeed = 50f;
+    int direction = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        //rotation of circle
+        transform.Rotate(new Vector3(0,0,direction*rotationSpeed*Time.deltaTime),Space.Self);
+    }
+}
---
+++ AA-clone/Assets/Scripts/pin_script.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pin_script : MonoBehaviour
+{
+    bool fire = false;
+    bool collisionWithRotator = false;
+    float speed = 20f;
+    private Rigidbody2D rb;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        rb= this.gameObject.GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (!collisionWithRotator)
+        {
+            rb.MovePosition(rb.position + Vector2.up * speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.tag);
+        if (collision.tag== "center_circle")
+        {
+            Debug.Log("in");
+            transform.SetParent(collision.transform);
+            collisionWithRotator = true;
+        }
+    }
+}
